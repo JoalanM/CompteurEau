05dced1b79db6e8470ee1492109f0102f7d01cab
JoalanM <81615097+JoalanM@users.noreply.github.com>
2022-03-10 08:56:33 +0100

    Add files via upload

--- DATABASE/EAU.sql
+++ DATABASE/EAU.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 08 mars 2022 à 10:35
+-- Généré le : jeu. 10 mars 2022 à 08:55
 -- Version du serveur :  10.3.34-MariaDB-0ubuntu0.20.04.1
 -- Version de PHP : 7.4.3
 
@@ -67,6 +67,25 @@ INSERT INTO `RELAIS` (`id`, `etat`, `date`) VALUES
 (4, 'ON', '2022-03-08 07:16:49'),
 (5, 'ON', '2022-03-08 07:16:51');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `VACANCES`
+--
+
+CREATE TABLE `VACANCES` (
+  `id` int(11) NOT NULL,
+  `vac` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `VACANCES`
+--
+
+INSERT INTO `VACANCES` (`id`, `vac`) VALUES
+(1, 'oui'),
+(2, 'non');
+
 --
 -- Index pour les tables déchargées
 --
@@ -78,6 +97,12 @@ ALTER TABLE `CONSOMMATION`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `VACANCES`
+--
+ALTER TABLE `VACANCES`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -86,6 +111,12 @@ ALTER TABLE `CONSOMMATION`
 --
 ALTER TABLE `CONSOMMATION`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `VACANCES`
+--
+ALTER TABLE `VACANCES`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
