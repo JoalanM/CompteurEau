bcdb5b15228c9eba5f11377becf6f357898dcd45
JoalanM <81615097+JoalanM@users.noreply.github.com>
2022-03-15 11:59:36 +0100

    Add files via upload

--- ScriptVacances.php
+++ ScriptVacances.php
@@ -1,5 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
+</head>
+<body>
 <?php
-
+	
 	//importation classes phpMQTT
 	require("classes/phpMQTT.php");
 	//**************************** */
@@ -94,4 +103,7 @@
 
 
 ?>
+</body>
+</html>
+
 
