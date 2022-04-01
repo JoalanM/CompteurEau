a2ef7355c9fe213f6295c72d6d78d5c9a394c012
JoalanM <81615097+JoalanM@users.noreply.github.com>
2022-04-01 11:29:27 +0200

    Add files via upload

---
+++ fuite.php
@@ -0,0 +1,81 @@
+<?php
+			
+            //importation classes phpMQTT
+            require("classes/phpMQTT.php");
+            //**************************** */
+			require('presence02.php');
+            //Informations de connexion au serveur Mosquitto
+            $server = "192.168.5.74";
+            $port = 1883;
+            $username_mqtt = "esp8266";
+            $password_mqtt = "esp8266";
+            $client_id = "PhpMqtt";
+            //************************* */
+			
+			//***********Information de connexion BASE DE DONNEE***************** */
+			$servername = "localhost";
+			$username_db = "snir";
+			$password_db = "snir";
+			$dbname = "EAU";
+			//******************************************************************* */
+			
+
+			// Création de la connexion BD
+			$conn = new mysqli($servername, $username_db, $password_db, $dbname);
+			// Verification de la connexion
+			if ($conn->connect_error) 
+			{
+				die("Echec de connexion : " . $conn->connect_error);
+			}
+			
+			//************************************************************************** */
+			//************************************************************************** */
+
+
+			$date_depart2 = $date_depart;
+			$presence= $reponse;
+
+			if($presence=="non")
+			{
+
+			
+				
+				//***************************Récupérer derniere valeure ************************************
+				$sql = "SELECT consommation FROM CONSOMMATION WHERE date>= $date_depart2 ";
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) 
+				{
+					// output data of each row
+					while($row = $result->fetch_assoc()) 
+					{
+						echo "<br>";
+						echo $row["consommation"] ." " ;
+						$consommation = $consommation + $row["consommation"];
+						
+
+						
+					}
+					echo "<br>" .$consommation ."<br>";
+					if($consommation >=10)
+					{
+						echo "ATTENTION FUITE EVENTUELLE ";
+						$destinataire = "myrtil.joalan1@gmail.com";
+						$sujet = "Consommation";
+						$message = "consommation detectée en votre abs";
+						$headers = "From : joalan.myrtil1@gmail.com";
+						mail($destinataire, $sujet, $message, $headers); 
+					} 
+				} 
+				else 
+				{
+					echo "0 résultat";
+				}
+				$conn->close();
+				//************************************************************************************ */
+
+				//************************************************************************** */
+				//************************************************************************** */
+
+			}
+		?>
